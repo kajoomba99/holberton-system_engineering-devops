fcf585e4a86f0e6c4259b6e62f4fcba5ad3c0383
steven brand <steven.brand1999@gmail.com>
2020-09-14 19:35:01 -0500

    task #2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""module 1-export_to_CSV
+    with the function todoUserProgressToCSV"""
+import csv
+import json
+import requests
+import sys
+
+
+def todoUserProgressToJSON(user_id: int) -> None:
+    """export information about user TODO list progress in the CSV format."""
+    URL = "https://jsonplaceholder.typicode.com"
+    endpoint_user = "{}/users/{}".format(URL, user_id)
+    endpoint_tasks = "{}/todos?userId={}".format(URL, user_id)
+
+    user_request = requests.get(endpoint_user)
+    user_response = user_request.json()
+    username = user_response.get("username")
+
+    task_request = requests.get(endpoint_tasks)
+    task_response = task_request.json()
+
+    list_tasks = []
+    for task in task_response:
+        title = task.get("title")
+        task_status = task.get("completed")
+        dict_task = {
+            "task": title,
+            "completed": task_status,
+            "username": username
+        }
+        list_tasks.append(dict_task)
+    progress_dict = {"{}".format(user_id): list_tasks}
+    with open("{}.json".format(user_id), "w") as json_file:
+        json.dump(progress_dict, json_file)
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    todoUserProgressToJSON(user_id)
